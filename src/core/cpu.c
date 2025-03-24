6f387b86acb611de82ae195aea52081dc6e69f3b
David Cerdeira <davidmcerdeira@gmail.com>
2025-03-24 11:45:39 +0000

    Fix overflow

--- src/core/cpu.c
+++ src/core/cpu.c
@@ -46,7 +46,7 @@ void cpu_init(cpuid_t cpu_id, paddr_t load_addr)
         cpu_sync_init(&cpu_glb_sync, platform.cpu_num);
 
         ipi_cpumsg_handler_num =
-            (size_t)(_ipi_cpumsg_handlers_id_end - _ipi_cpumsg_handlers_id_start);
+            (size_t)(_ipi_cpumsg_handlers_id_end - _ipi_cpumsg_handlers_id_start) / sizeof(size_t);
         for (size_t i = 0; i < ipi_cpumsg_handler_num; i++) {
             ((size_t*)_ipi_cpumsg_handlers_id_start)[i] = i;
         }

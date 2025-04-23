6a0c38733a993de08fcdab867f55347f61d8cd2c
Daniel Oliveira <drawnpoetry@gmail.com>
2025-04-23 14:23:20 +0100

    feat(armv8m): add armv8m arch
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

---
+++ src/arch/armv8m/inc/arch/mem.h
@@ -0,0 +1,49 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
+
+#ifndef MEM_ARCH_H
+#define MEM_ARCH_H
+
+#include <crossconhyp.h>
+#include <arch/sysregs.h>
+#include <arch/mpu.h>
+
+typedef mpu_flags_t mem_flags_t;
+
+#define PTE_FLAGS(_rbar, _rlar) \
+    ((mem_flags_t){             \
+        .rbar = (_rbar),        \
+        .rlar = (_rlar),        \
+    })
+
+#define PTE_INVALID PTE_FLAGS(0, 0)
+
+// TODO:ARMV8M - Use separate attributes for code and data
+#define PTE_HYP_FLAGS \
+    PTE_FLAGS(MPU_RBAR_XN | MPU_RBAR_AP_RW_PLVL | MPU_RBAR_SH_NS, MPU_RLAR_ATTR(1) | MPU_RLAR_EN)
+#define PTE_HYP_FLAGS_CODE \
+    PTE_FLAGS(MPU_RBAR_AP_RO_PLVL | MPU_RBAR_SH_NS, MPU_RLAR_ATTR(1) | MPU_RLAR_EN)
+#define PTE_HYP_DEV_FLAGS \
+    PTE_FLAGS(MPU_RBAR_XN | MPU_RBAR_AP_RW_PLVL | MPU_RBAR_SH_NS, MPU_RLAR_ATTR(2) | MPU_RLAR_EN)
+
+// TODO:ARMV8M - We are missing flags to distinguish flags for mem regions RX or RW
+
+#define PTE_VM_FLAGS     PTE_FLAGS(0, SAU_RLAR_EN)
+#define PTE_VM_DEV_FLAGS PTE_FLAGS(0, SAU_RLAR_EN)
+#define PTE_VM_HC_FLAGS  PTE_FLAGS(0, SAU_RLAR_EN | SAU_RLAR_NSC)
+
+#define MPU_ARCH_MAX_NUM_ENTRIES \
+    (8) // TODO:ARMV8M - This is implementation-def so it should be defined in the platform
+#define SAU_ARCH_MAX_NUM_ENTRIES \
+    (8) // TODO:ARMV8M - This is implementation-def so it should be defined in the platform
+
+static inline size_t mpu_granularity(void)
+{
+    return (size_t)PAGE_SIZE;
+}
+
+bool mpu_arch_perms_compatible(mem_flags_t perms1, mem_flags_t perms2);
+
+#endif /* MEM_ARCH_H */
---
+++ src/sdees/sdGPOS/arch/armv8m/sdgpos.c
@@ -0,0 +1,57 @@
+#include <hypercall.h>
+#include <vmstack.h>
+#include <config.h>
+#include "types.h"
+#include "vmm.h"
+#include "arch/sdgpos.h"
+
+// #define SMCC64_BIT   (0x40000000)
+
+//  #define SMCC_E_NOT_SUPPORTED  (-1)
+// #define SMCC32_FID_VND_HYP_SRVC (0x86000000)
+// #define SMCC64_FID_VND_HYP_SRVC (SMCC32_FID_VND_HYP_SRVC  | SMCC64_BIT)
+// #define SMCC_FID_FN_NUM_MSK (0xFFFF)
+
+static int64_t sdgpos_smc_handler(struct vcpu* vcpu, uint64_t smc_fid)
+{
+    UNUSED_ARG(smc_fid);
+    UNUSED_ARG(vcpu);
+    return -1;
+}
+
+static int64_t sdgpos_hvc_handler(struct vcpu* vcpu, uint64_t smc_fid)
+{
+    UNUSED_ARG(smc_fid);
+    UNUSED_ARG(vcpu);
+    return -1;
+}
+
+static struct hndl_smc smc = {
+    /* CROSSCON TODO: obtain this to decide whether to invoke handler early on */
+    .end = 0xffff0000,
+    .start = 0x00000000,
+    .handler = sdgpos_smc_handler,
+};
+
+static struct hndl_hvc hvc = {
+    /* CROSSCON TODO: obtain this to decide whether to invoke handler early on */
+    .handler = sdgpos_hvc_handler,
+};
+
+bool sdgpos_arch_setup(struct vm* vm)
+{
+    int64_t ret = 0;
+
+    if (vm == NULL) {
+        return -1;
+    }
+
+    /* CROSSCON TODO: check config structure or something to check if this VMs wants tz
+     * to handle its events */
+    if (vm->type == 0) {
+        vm_hndl_smc_add(vm, &smc);
+        vm_hndl_hvc_add(vm, &hvc);
+    }
+
+    return ret;
+}

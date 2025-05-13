876718bc435b66514398d034f26649993f2dff40
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2025-05-14 00:24:44 +0200

    fix(core/vm): fix info on HV init
    VA is used twice instead of size property.
    
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- src/core/vm.c
+++ src/core/vm.c
@@ -331,7 +331,7 @@ static void vm_init_dev(struct vm* vm, const struct vm_config* vm_config)
     for (size_t i = 0; i < vm_config->platform.dev_num; i++) {
         struct vm_dev_region* dev = &vm_config->platform.devs[i];
         INFO("VM %d adding MMIO region, VA: 0x%lx size: 0x%lx mapped at 0x%lx\n", vm->id, dev->va,
-            dev->va, dev->pa);
+            dev->size, dev->pa);
 
         if (DEFINED(MMIO_SLAVE_SIDE_PROT)) {
             vm_arch_allow_mmio_access(vm, dev);

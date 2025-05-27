62d20d71a01a06ce735ef0d3fd0201a5829a0c70
Daniel Oliveira <drawnpoetry@gmail.com>
2025-05-27 17:13:37 +0100

    fix(armv8m): fix uncompatible types
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/sdees/sdGPOS/arch/armv8m/sdgpos.c
+++ src/sdees/sdGPOS/arch/armv8m/sdgpos.c
@@ -12,14 +12,14 @@
 // #define SMCC64_FID_VND_HYP_SRVC (SMCC32_FID_VND_HYP_SRVC  | SMCC64_BIT)
 // #define SMCC_FID_FN_NUM_MSK (0xFFFF)
 
-static long sdgpos_smc_handler(struct vcpu* vcpu, uint64_t smc_fid)
+static int64_t sdgpos_smc_handler(struct vcpu* vcpu, unsigned long smc_fid)
 {
     UNUSED_ARG(smc_fid);
     UNUSED_ARG(vcpu);
     return -1;
 }
 
-static long sdgpos_hvc_handler(struct vcpu* vcpu, uint64_t smc_fid)
+static long sdgpos_hvc_handler(struct vcpu* vcpu, unsigned long smc_fid)
 {
     UNUSED_ARG(smc_fid);
     UNUSED_ARG(vcpu);
